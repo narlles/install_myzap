ca0a733d00a963b24c37508ee52c4fcbd07e546d
owenzap <103150476+owenzap@users.noreply.github.com>
2022-06-03 01:35:20 -0300

    Update myzap.sh

--- myzap.sh
+++ myzap.sh
@@ -7,11 +7,11 @@
 
 	function msg() {
 		msg=$1
-		whiptail --title "Instalacao API MYZAP by (Comunidade de Software Livre)" --msgbox "$msg" --fb 10 70;
+		whiptail --title "Instalacao API MYZAP by OwenZap - (27 99263-3735)" --msgbox "$msg" --fb 10 70;
 	}
 
 
-	whiptail --title "Instalacao API MYZAP by (Comunidade de Software Livre)" --msgbox "Aperte ENTER para iniciar a instalacao" --fb 10 70
+	whiptail --title "Instalacao API MYZAP by OwenZap - (27 99263-3735)" --msgbox "Aperte ENTER para iniciar a instalacao" --fb 10 70
 	[[ $(whoami) != 'root' ]] && {
 		whiptail --title "Para continuar a instalacao, logue-se como ROOT" --msgbox "comando: su" --fb 10 70
 		exit;
@@ -95,7 +95,7 @@
 		fi
 	done
 
-	msg "Vai tomar um cafe, fumar um cigarro que a instalacao vai comecar!";
+	msg "Vai tomar um cafe, beber uma gelada, fumar um cigarro que a instalacao vai comecar!";
 
 
 apt install -y curl nano git gconf-service libasound2 libatk1.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget build-essential apt-transport-https libgbm-dev
